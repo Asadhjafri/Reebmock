513d527f57628dc73a48a494a7e1fd1300fcfd16
Asadhjafri <asadhjafri@gmail.com>
2021-06-07 13:34:23 -0400

    create Navbar component and menu items for navbar. map through array of menuitem to target each part of menu items.

---
+++ src/components/Navbar/MenuItems.jsx
@@ -0,0 +1,44 @@
+export const MenuItems = [
+    {
+        title:'Home',
+        url: "#",
+        cName: "nav-links"
+
+    },
+    {
+        title:'Mens',
+        url: "#",
+        cName: "nav-links"
+
+    },
+    {
+        title:'Womens',
+        url: "#",
+        cName: "nav-links"
+
+    },
+    {
+        title:'Shoes',
+        url: "#",
+        cName: "nav-links"
+
+    },
+    {
+        title:'Accessories',
+        url: "#",
+        cName: "nav-links"
+
+    },
+    {
+        title:'Culture',
+        url: "#",
+        cName: "nav-links"
+
+    },
+    {
+        title:'Sign Up',
+        url: "#",
+        cName: "nav-links-mobile"
+
+    }
+]
