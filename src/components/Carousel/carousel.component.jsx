ea3392e1659716840435308f7897672e81e753dd
Asadhjafri <asadhjafri@gmail.com>
2021-06-14 13:58:26 -0400

    import and export components appropriatly. Set images for react responsive carousel.

--- src/components/Carousel/carousel.component.jsx
+++ src/components/Carousel/carousel.component.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function CarouselComponent() {
     return (
-        <div>
-            <h3>Carousel in React</h3>
+        <div className="carousel-wrapper">
+            <Carousel infiniteLoop useKeyboardArrows autoPlay>
+                <div>
+                    <img src="../Reebok_1.jpeg" />
+                </div>
+                <div>
+                    <img src="../Reebok_2.jpeg" />
+                </div>
+                <div>
+                    <img src="../Reebok_3.jpeg" />
+                </div>
+            </Carousel>
         </div>
     );
-}
+};
+
